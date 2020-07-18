5adfd8223b32d468f784728792bb9efe88dcbdf1
Costa <costa365@gmail.com>
2020-07-18 18:26:19 +0300

    Updated emails

--- node-backend/src/services/email.js
+++ node-backend/src/services/email.js
@@ -15,13 +15,13 @@ var mailOptions = {
   from: 'notesapp001@gmail.com',
   to: 'dummy@dummy.com',
   subject: '',
-  text: ''
+  html: ''
 };
 
 const sendWelcomeMail = (address, user) => {
   mailOptions.to = address;
   mailOptions.subject = 'Welcome to Notes365!';
-  mailOptions.text = 'Hey ' + user + ', thanks for registering with Notes365. You can now log in at https://notes365.tk and start creating notes.';
+  mailOptions.html = '<img src="https://notes365.tk/logo.png" alt="Notes365 Logo"><hr /><p>Hey <b>' + user + '</b>, thanks for registering with Notes365. You can now log in at <a href="https://notes365.tk">Notes365</a> and start creating notes.</p><hr />';
   transporter.sendMail(mailOptions, function(error, info){
     if (error) {
         console.log(error);
@@ -35,7 +35,8 @@ const sendResetMail = (address, token) => {
   mailOptions.to = address;
   mailOptions.subject = 'Password Reset Request';
   mailOptions.text = 'You can reset using: https://notes365.tk/reset?token=' + token;
-  
+  mailOptions.html = '<img src="https://notes365.tk/logo.png" alt="Notes365 Logo"><hr /><p>Hey <b>' + user + '</b>, you can reset your password using <a href="https://notes365.tk/reset?token=' + token + '">this link</a>.</p><hr />';
+
   transporter.sendMail(mailOptions, function(error, info){
     if (error) {
         console.log(error);
