33c31a4ba29a9c2a08884f814f0de1d893e54d45
Costa <costa365@gmail.com>
2026-09-13 13:52:57 +0000

    Fixed build error

--- react-frontend/src/components/Reset.js
+++ react-frontend/src/components/Reset.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import Swal from 'sweetalert2';
 import UserService from './UserService';
 import './Styles.css';
-import { useSearchParams } from 'react-router-dom';
 
 export default class Reset extends Component {
   constructor(props) {
